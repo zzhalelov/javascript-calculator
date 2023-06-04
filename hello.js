3cba3f2c8ff995280da024a98523f97ec27e3646
zzhalelov <zzhalelov@gmail.com>
2023-06-05 01:23:44 +0600

    2nd commit

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("hello");
+let firstValue = document.getElementById('firstValue');
+let secondValue = document.getElementById('secondValue');
+let result = firstValue + secondValue;
+function clicked() {
+    console.log(result);
+}
